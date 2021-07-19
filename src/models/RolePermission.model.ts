aeaa49ae31aac9f0310ec6369b705468ee0f0886
darkgraycat <jburyanov@gmail.com>
2021-07-19 11:30:57 +0300

    undo

--- src/models/RolePermission.model.ts
+++ src/models/RolePermission.model.ts
@@ -2,11 +2,6 @@ import { Column, ForeignKey, Model, Table } from "sequelize-typescript";
 import Permission from "./Permission.model";
 import Role from "./Role.model";
 
-/*
-  same as original example
-  https://github.com/RobinBuschmann/sequelize-typescript-example/blob/master/lib/models/MovieActor.ts
-*/
-
 @Table
 export default class RolePermission extends Model<RolePermission> {
   @ForeignKey(() => Role)
